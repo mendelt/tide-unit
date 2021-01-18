3bace27936c91887c3ec6acaf758b7b796ed86ec
Mendelt Siebenga <mendelt@mendelt.nl>
2021-01-18 22:39:40 +0100

    setup basic test syntax

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,42 @@
+use tide::Endpoint;
+
+pub fn unit<State: Clone + Send + Sync + 'static>(state: State) -> TideUnitBuilder<State> {
+    TideUnitBuilder { _state: state }
+}
+
+pub struct TideUnitBuilder<State> {
+    _state: State,
+}
+
+impl<State: Clone + Send + Sync + 'static> TideUnitBuilder<State> {
+    pub fn param(self, _param: &str, _value: &str) -> Self {
+        self
+    }
+
+    pub fn query(self, _key: &str, _value: &str) -> Self {
+        self
+    }
+
+    pub fn ext<T>(self, _ext: T) -> Self {
+        self
+    }
+
+    pub fn test_endpoint(&self, _endpoint: impl Endpoint<State>) {
+
+    }
+}
+
 #[cfg(test)]
-mod tests {
+mod when_testing_an_endpoint {
+    use super::*;
+    use tide::{Request, Result};
+
+    async fn endpoint(_request: Request<()>) -> Result {
+        Ok("result".into())
+    }
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        unit(()).param("param1", "value1").param("param2", "value2").query("item1", "value").test_endpoint(endpoint)
     }
 }
